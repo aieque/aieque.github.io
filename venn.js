7b79157288f2f05ae661a68f60678feffc46d098
aieque <sixten.hugosson@gmail.com>
2018-11-15 08:18:43 +0100

    Create venn.js

---
+++ venn.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById("canvas")
+
+function Button(text, info) {
+	this.text = text;
+	this.info = info;
+	this.render = function() {
+		
+	};
+}
+btn1 = Button("Lite Text", "Lite mera text");
